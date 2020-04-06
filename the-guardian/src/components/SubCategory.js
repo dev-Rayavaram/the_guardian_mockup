532a4af6a520cda1daed05bd2714c2f6046cdc13
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-05 19:19:17 -0500

    data fetched from api

--- the-guardian/src/components/SubCategory.js
+++ the-guardian/src/components/SubCategory.js
@@ -3,11 +3,16 @@ import React,{Component} from 'react';
 
 class SubCategory extends Component
     {
+      constructor(props){
+        super(props)
+        this.state={
+          sectionName:this.props.subCategory
+        }
+      }
       render(){
         return (
           <div className="App">
-            <h1>Welcome to SubCategory</h1>
-      
+            <h1>{this.props.subCategory}</h1>
           </div>
         );
       }
