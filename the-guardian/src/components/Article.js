05aa0a93c5e9bd1b2ddc27ecb48c40441fb16dd0
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-05 22:51:43 -0500

    fixed warnings in Article.js

--- the-guardian/src/components/Article.js
+++ the-guardian/src/components/Article.js
@@ -4,6 +4,11 @@ import React,{Component} from 'react';
 class Article extends Component
     {
       render(){
+        let items = this.props.data;
+        for(let i=0;i<items.length;i++){
+          console.log("inside article",i);
+          console.log(items[i])
+        }
         return (
           <div className="App">
             <h1>Welcome to Category</h1>
