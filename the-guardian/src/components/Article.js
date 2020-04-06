109b0ff9a24d175f8e481d6ae3b1bc7b9d8195c2
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-06 00:19:46 -0500

    titles added for each div

--- the-guardian/src/components/Article.js
+++ the-guardian/src/components/Article.js
@@ -4,14 +4,10 @@ import React,{Component} from 'react';
 class Article extends Component
     {
       render(){
-        let items = this.props.data;
-        for(let i=0;i<items.length;i++){
-          console.log("inside article",i);
-          console.log(items[i])
-        }
+       
         return (
-          <div className="App">
-            <h1>Welcome to Category</h1>
+          <div className='box2'>
+            <h1>{this.props.data}</h1>
       
           </div>
         );
