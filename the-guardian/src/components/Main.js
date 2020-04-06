8ef9d3dc1633ac4bc9486432ccb45af6c938aec8
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-05 22:19:54 -0500

    updated main component

--- the-guardian/src/components/Main.js
+++ the-guardian/src/components/Main.js
@@ -1,14 +1,19 @@
 import React,{Component} from 'react';
- 
+ import image from '../assets/images/1208.jpg'
 
 class Main extends Component
     {
       render(){
+        const openInNewTab=(url)=> {
+           window.open(url, '_blank');
+        }
         return (
-          <div className="App">
-            <h1>Welcome to Category</h1>
-      
-          </div>
+          <React.Fragment>
+           <h1>{this.props.main.webTitle}</h1><caption>{this.props.main.webPublicationDate}</caption>
+           <div>
+              <img src={image} alt="main" width='400px' height='400px'></img>
+           </div>
+          </React.Fragment>
         );
       }
         
